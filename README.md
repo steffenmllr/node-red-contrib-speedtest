511e112fab39948ddb25a1347cd85b418907d72b
Steffen Müller <steffen@mllrsohn.com>
2017-04-22 10:59:09 +0200

    init

---
+++ README.md
@@ -0,0 +1,25 @@
+# node-red-contrib-speedtest
+
+[![NPM version][npm-image]][npm-url]
+
+> A Node-RED [speedtest.net](https://github.com/ddsol/speedtest.net) node, for testing your internet speed
+
+## Installation
+
+Install `node-red-contrib-speedtest` using [npm](https://www.npmjs.com/):
+
+```bash
+npm install --save node-red-contrib-speedtest
+```
+
+## Usage
+
+To use the node, launch Node-RED (see [running Node-RED](http://nodered.org/docs/getting-started/running.html) for help getting started).
+Any input will request a speed-test. The output looks like the [original data output](https://github.com/ddsol/speedtest.net#data)
+
+## License
+
+MIT © [Steffen Müller](http://steffen.io)
+
+[npm-url]: https://npmjs.org/package/node-red-contrib-speedtest
+[npm-image]: https://badge.fury.io/js/node-red-contrib-speedtest.svg
---
+++ speedtest.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var speedTest = require('speedtest-net');
+
+module.exports = exports = function(RED) {
+    function SpeedTest(config) {
+        var timeout = config.maxTime || 5 * 1000;
+        RED.nodes.createNode(this, config);
+
+        this.on('input', msg => {
+            this.status({ fill: 'yellow', shape: 'dot', text: 'Requesting' });
+            var test = speedTest({ maxTime: config.maxTime });
+
+            test.on('data', data => {
+                var reponse = Object.assign({}, data, { config: config });
+                this.status({});
+                this.send({ payload: reponse });
+            });
+
+            test.on('error', err => {
+                this.status({ fill: 'red', shape: 'dot', text: err.message });
+                this.error(err, msg);
+            });
+        });
+    }
+
+    RED.nodes.registerType('speedtest', SpeedTest);
+};
