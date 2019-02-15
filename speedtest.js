e482ced4e0b3ddd6095d8f4e7840e4b020b4c4a5
war6000 <14223774+war6000@users.noreply.github.com>
2019-02-15 14:59:35 -0800

    added ServerId
    added serverID and default Los Angeles AT&T default

--- speedtest.js
+++ speedtest.js
@@ -5,11 +5,12 @@ var speedTest = require('speedtest-net');
 module.exports = exports = function(RED) {
     function SpeedTest(config) {
         var timeout = config.maxTime || 5 * 1000;
+        var servid = config.serverId || 5031;
         RED.nodes.createNode(this, config);
 
         this.on('input', msg => {
             this.status({ fill: 'yellow', shape: 'dot', text: 'Requesting' });
-            var test = speedTest({ maxTime: config.maxTime });
+            var test = speedTest({ maxTime: config.maxTime, serverId: config.serverId });
 
             test.on('data', data => {
                 var reponse = Object.assign({}, data, { config: config });
